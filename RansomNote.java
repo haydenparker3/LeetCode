cdc3b2ef59b67820d68b503b9516325db59afd19
haydenparker3 <parkerhomehayden@gmail.com>
2022-07-16 15:12:05 -0500

    added problem

---
+++ RansomNote.java
@@ -0,0 +1,22 @@
+/*
+ * 383. Ransom Note
+ */
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        
+        for(int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0; i < ransomNote.length(); i++) {
+            if(count[ransomNote.charAt(i) - 'a'] == 0)
+                return false;
+            else
+                count[ransomNote.charAt(i) - 'a']--;
+        }
+        
+        return true;
+    }
+}
